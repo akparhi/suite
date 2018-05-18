6dea21ec77eef42f5f8f96125bb6faecb7813eca
_pariah <anand.parhi1337@gmail.com>
2018-05-18 15:32:41 +0530

    use hashhistory for github pages

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/createHashHistory';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
